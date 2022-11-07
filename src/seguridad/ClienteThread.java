8724a9a4cd6d2d7d94ad02f6ecd7c67db3bb78f2
Alejandro  Salgado <78111596+Alejandro1400@users.noreply.github.com>
2022-11-06 23:29:58 -0500

    Cambios cliente thread

--- src/seguridad/ClienteThread.java
+++ src/seguridad/ClienteThread.java
@@ -1,11 +1,16 @@
 package seguridad;
 
 import java.io.BufferedReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.net.Socket;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.util.Random;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
@@ -16,10 +21,8 @@ public class ClienteThread extends Thread{
 	
 	
 	//Atributos
-	private PrintWriter escritor = null;
-	private BufferedReader lector = null;
-	private int idCliente = -1;
-	private BufferedReader stdIn = null;
+	private int idCliente;
+	private Socket sc;
 	private SecurityFunctions f;
 	private String dlg;
 	private BigInteger p;
@@ -29,11 +32,9 @@ public class ClienteThread extends Thread{
 	//Metodo constructor 
 	//Recibe el lector de lo que escribe el cliente, de lo que devuelve el servidor, escritor de lo que
 	//envia al servidor, y el id de cliente.
-	public ClienteThread (BufferedReader pStdIn, BufferedReader pLector, PrintWriter pEscritor, int pIdCliente)
+	public ClienteThread (Socket pSc, int pIdCliente)
 	{
-		stdIn = pStdIn;
-		lector = pLector;
-		escritor = pEscritor;
+		sc = pSc;
 		idCliente = pIdCliente;
 		
 		//Se usa dlg porque este afecta a la creacion de la llave privada 
@@ -55,7 +56,9 @@ public class ClienteThread extends Thread{
 		
 		try 
 		{
-			
+			//Crea la comunicacion entre el servidor y el cliente 
+			PrintWriter escritor = new PrintWriter(sc.getOutputStream(), true);
+			BufferedReader lector = new BufferedReader(new InputStreamReader(sc.getInputStream()));
 			//Inicialmente necesitamos tener presente que el thread de cliente conoce la llave del servidor
 			//por lo que se crea igual a como se creo en el servidor
 			PublicKey publicaServidor = f.read_kplus("datos_asim_srv.pub",dlg);
@@ -79,11 +82,11 @@ public class ClienteThread extends Thread{
 			
 			//Primero se genera el mismo mensaje que se hizo en la firma a partir de g, p y G^x, 
 			//para llevar a cabo la verificacion
-			String msj = g.toString()+","+p.toString()+","+linea;
+			String msj = g.toString()+","+p.toString()+","+g2x.toString();
 			
 			//Luego tiene que recibir la firma 
 			linea = lector.readLine();
-			byte[] byteauthentication = linea.getBytes();
+			byte[] byteauthentication = str2byte(linea);
 			
 			//Ahora verifica la firma a partir de la funcion definida en SecurityFunction
 			boolean respAut = f.checkSignature(publicaServidor, byteauthentication, msj);
@@ -106,7 +109,7 @@ public class ClienteThread extends Thread{
 	    		String str_valor_comun = valor_comun.toString();
 	    		
 	    		//Enviamos G^y al servidor para que pueda crear la llave maestra 
-	    		escritor.println("str_valor_comun");
+	    		escritor.println(str_valor_comun);
 	    		
 	    		//Calculamos la llave maestra
 	    		// computing (G^x)^y mod N
@@ -115,7 +118,7 @@ public class ClienteThread extends Thread{
 	    		System.out.println(dlg + " llave maestra: " + str_llave);
 				
 	    		//A partir de la llave maestra se calcula la llave simetrica de cifrado y HMAC
-	    		SecretKey sk_srv = f.csk1(str_llave);
+	    		SecretKey sk_cliente = f.csk1(str_llave);
 				SecretKey sk_mac = f.csk2(str_llave);
 				
 				//De igual manera, se calcula iv_1
@@ -124,19 +127,68 @@ public class ClienteThread extends Thread{
 				IvParameterSpec ivSpec1 = new IvParameterSpec(iv1);
 				
 				
-				//Enviar un mensaje cifrado
 				
-				//Primero el cliente ingresa un numero 
-				System.out.println("Ingrese un numero: ");
-				linea = stdIn.readLine();
+				//Enviar un mensaje cifrado y el HMAC
 				
-				try
-				{
+				//Primero se envia el numero random 
+				Random aleatorio = new Random();
+				Integer random = aleatorio.nextInt((Integer.MAX_VALUE-1)+1);
+				String valor = String.valueOf(random);
+				
+				
+				//Enviar mensaje cifrado con la llave simetrica de cifrado
 					
+				//Pasamos a bytes el valor ingresado por la persona
+				byte[] byte_valor = valor.getBytes();
+					
+				//Generamos el mensaje cifrado para la consulta
+				byte[] consulta = f.senc(byte_valor, sk_cliente,ivSpec1, "Cliente");
+				//Genera el hmac del mensaje que se envia cifrado
+		        byte [] hmac = f.hmac(byte_valor, sk_mac);
+		        //Los convierte a string para ser enviados
+		        String m1 = byte2str(consulta);
+		        String m2 = byte2str(hmac);
+		        //Envia al servidor
+		        escritor.println(m1);
+		        escritor.println(m2);
+		        escritor.println(str_iv1);
+		        
+		        
+		        //El cliente recibe la respuesta de si fue correcta la comunicacion del mensaje cifrado
+		        //con su HMAC
+		        linea = lector.readLine();
+    			if (linea.compareTo("OK")==0) {
+    				System.out.println("==========> client sends matching query and MAC");
+    			} else if (linea.compareTo("ERROR")==0) {
+    				System.out.println("==========> failed client sends matching query and MAC");
+    				exito = false;
+    			}
+		        
+		        
+		        
+		        //Recibir el mensaje de respuesta, hmac y iv a partir del cual revisa que se haya cumplido
+		        //la verificacion
+		        String str_rtaconsulta = lector.readLine();
+				String str_rtamac = lector.readLine();
+				String str_iv2 = lector.readLine();
+				byte[] byte_rtaconsulta = str2byte(str_rtaconsulta);
+				byte[] byte_mac = str2byte(str_rtamac);
+				
+				byte[] iv2 = str2byte(str_iv2);
+				IvParameterSpec ivSpec2 = new IvParameterSpec(iv2);
+		    	byte[] descifrado = f.sdec(byte_rtaconsulta, sk_cliente,ivSpec2);
+		    	boolean verificar = f.checkInt(descifrado, sk_mac, byte_mac);
+				System.out.println(dlg + "Integrity check:" + verificar);  
+				
+				if (verificar) {
+		    		System.out.println("==========> Test: passed Server sends matching query and MAC).");
+		    		escritor.println("OK");
 				}
-				
-				
-				
+				else
+				{
+					escritor.println("ERROR");
+					exito = false;
+				}
 				
 			}
 			else
@@ -146,12 +198,8 @@ public class ClienteThread extends Thread{
 			}
 			
 			
-			
-			
-			
-			
-			
-			
+			escritor.close();
+			lector.close();
 			
 		}
 		catch (Exception e) 
@@ -159,10 +207,36 @@ public class ClienteThread extends Thread{
 			e.printStackTrace(); 
 		}
 		
+		
+		try {
+			if (exito)
+		        System.out.println(dlg + "Finishing test: passed.");		
+		    else
+		       	System.out.println(dlg + "Finishing test: failed.");
+		        
+			sc.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 	
 	
 	//Metodos
+	
+	//Metodo que retorna el valor de string convertido en bytes
+	public byte[] str2byte( String ss)
+	{	
+		// Encapsulamiento con hexadecimales
+		byte[] ret = new byte[ss.length()/2];
+		for (int i = 0 ; i < ret.length ; i++) {
+			ret[i] = (byte) Integer.parseInt(ss.substring(i*2,(i+1)*2), 16);
+		}
+		return ret;
+	}
 	
 	//Metodo que retorna G^X, o en caso de cliente G^Y
 	private BigInteger G2Y(BigInteger base, BigInteger exponente, BigInteger modulo) {
@@ -193,13 +267,3 @@ public class ClienteThread extends Thread{
 	    return iv;
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
